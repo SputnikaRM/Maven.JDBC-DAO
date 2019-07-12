4da9776817474131291e77ee86d472ba722eeeee
sputnika <sputnika@zipcoders-MacBook-Pro-5.local>
2019-07-12 16:34:36 -0400

    update

---
+++ src/main/java/daos/Dao.java
@@ -0,0 +1,17 @@
+package daos;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+   public T findById(int id);
+
+    public List findAll();
+
+    public T update(T dto);
+
+    public T create(T dto);
+
+    public void delete(int id);
+
+}
---
+++ src/main/java/daos/Daoconcrete.java
@@ -0,0 +1,27 @@
+package daos;
+
+import java.util.List;
+
+public class Daoconcrete implements Dao{
+
+
+    public Object findById(int id) {
+        return null;
+    }
+
+    public List findAll() {
+        return null;
+    }
+
+    public Object update(Object dto) {
+        return null;
+    }
+
+    public Object create(Object dto) {
+        return null;
+    }
+
+    public void delete(int id) {
+
+    }
+}
