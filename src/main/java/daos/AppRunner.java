8db453f4e1b65d36cc34c7032bd9fb7e60ad80b1
sputnika <sputnika@MacBook-Pro-3.localdomain>
2019-07-12 23:58:35 -0400

    lab

--- src/main/java/daos/AppRunner.java
+++ src/main/java/daos/AppRunner.java
@@ -1,4 +1,23 @@
 package daos;
 
+import models.Student;
+
+import java.util.List;
+
 public class AppRunner {
+
+    private static Daoconcrete studentDao;
+
+    public static void main(String [] args){
+        Daoconcrete student = new Daoconcrete();
+
+
+
+     // run crud operations in main
+
+
+
+
+    }
+
 }
